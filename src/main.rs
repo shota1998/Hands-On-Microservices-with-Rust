38fb97820d5dbb77a51b26a5888f1fec6cde9f7e
Shota Sato <sugergg@gmail.com>
2022-09-14 08:59:36 +0900

    Finish.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@ use dotenv::dotenv;
 use hyper::{Body, Response, Server};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
-use log::{debug, info, trace};
+use log::{debug, info, trace, warn};
+use serde_derive::Deserialize;
 use std::env;
+use std::io::{self, Read};
+use std::fs::File;
+use std::net::SocketAddr;
+
+#[derive(Deserialize)]
+struct Config {
+    address: SocketAddr,
+}
 
 
 fn main() {
@@ -35,13 +44,30 @@ fn main() {
     trace!("Starting...");
 
 
+    let config = File::open("microservice.toml")
+                      .and_then(|mut file| {
+                        let mut buffer = String::new();
+                        file.read_to_string(&mut buffer)?;
+                        Ok(buffer)
+                      })
+                      .and_then(|buffer| {
+                        toml::from_str::<Config>(&buffer)
+                             .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+                      })
+                      .map_err(|err| {
+                        warn!("Can't read config file: {}", err);
+                      })
+                      .ok();
+
+
     let addr = matches.value_of("address")
                       .map(|s| s.to_owned())
                       .or(env::var("ADDRESS").ok())
-                      .unwrap_or_else(|| "127.0.0.1:8080".into())
-                      .parse()
-                      .expect("cant't parse ADDRESS variable");
-
+                      .and_then(|addr| addr.parse().ok())
+                      .or(config.map(|config| config.address))
+                      .or_else(|| Some(([127, 0, 0, 1], 8080).into()))
+                      .unwrap();
+                      
     debug!("Trying to bind server to address: {}", addr);
 
 
