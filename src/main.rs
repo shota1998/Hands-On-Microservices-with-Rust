9821c8af116afc2f800d498befefdeaae6ba9e94
Shota Sato <sugergg@gmail.com>
2022-09-25 18:57:25 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use hyper::{Body, Response, Server};
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
 
 fn main() {
-    let addr = ([127, 0, 0, 1], 8080).into();
-    let builder = Server::bind(&addr);
-    let server = builder.serve(|| {
-        service_fn_ok( |_| {
-            Response::new(Body::from("Rust Microservice"))
-        })
-    });
+    multiple();
+}
 
-    let server = server.map_err(drop);
-    hyper::rt::run(server);
+fn multiple() {
+    let (tx_sink, rx_stream) = mpsc::channel::<u8>(8);
+
 }
