8adfe6c3bb11265f39e0715ae2624a648dd461fb
Shota Sato <sugergg@gmail.com>
2022-09-25 21:10:55 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use failure::Error;
+use futures::{future, stream, Future, Stream, Sink, IntoFuture};
+use futures::sync::{mpsc, oneshot};
+use tokio::net::{UdpSocket, UdpFramed};
+use tokio::codec::LinesCodec;
+
+
+fn to_box<T>(fut: T) -> Box<dyn Future<Item=(), Error=()> + Send>
+where
+    T: IntoFuture,
+    T::Future: Send + 'static,
+    T::Item: 'static,
+    T::Error: 'static,
+{
+    let fut = fut.into_future().map(drop).map_err(drop);
+    Box::new(fut)
+}
 
 fn main() {
     multiple();
@@ -6,4 +24,23 @@ fn main() {
 fn multiple() {
     let (tx_sink, rx_stream) = mpsc::channel::<u8>(8);
 
+    let receiver = rx_stream.fold(0, |acc, value| {
+        future::ok(acc + value)
+    }).map(|x| {
+        println!("Calculated: {}", x);
+    });
+
+    let send_1 = tx_sink.clone().send(1);
+    let send_2 = tx_sink.clone().send(2);
+    let send_3 = tx_sink.clone().send(3);
+
+    let execute_all = future::join_all(vec![
+        to_box(receiver),
+        to_box(send_1),
+        to_box(send_2),
+        to_box(send_3),
+    ]).map(drop);
+
+    drop(tx_sink);
+    tokio::run(execute_all);
 }
